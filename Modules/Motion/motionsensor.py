72dfbb239b70e4401ededb74de1861501e195391
Juan <juanpaez@utexas.edu>
2021-02-01 00:22:41 -0500

    working motion sensor test

--- Modules/Motion/motionsensor.py
+++ Modules/Motion/motionsensor.py
@@ -1,17 +1,19 @@
 import RPi.GPIO as GPIO
 import time
+from datetime import datetime
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11, GPIO.IN)         #Read output from PIR motion sensor
+i = 0
 
 while True:
-    i=GPIO.input(3)
+    i=GPIO.input(11)
+    print(i)
     if i==0:                 #When output from motion sensor is LOW
-        print "No intruders",i
-        GPIO.output(3, 0)  #Turn OFF LED
-        time.sleep(0.1)
+        print(f"No intruders: {datetime.now()}")
+        time.sleep(0.5)
     elif i==1:               #When output from motion sensor is HIGH
-        print "Intruder detected",i
-        time.sleep(0.1)
+        print(f"Intruder detected: {datetime.now()}")
+        time.sleep(0.5)
 
