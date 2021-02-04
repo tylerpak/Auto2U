5012321a3b9a2abfcdb4fb2418fa75ff19fb03db
Juan Paez <juanfranciscopaez@gmail.com>
2021-02-04 17:09:00 -0600

    outline of main program

---
+++ auto2u.py
@@ -0,0 +1,18 @@
+# Main Auto2U program
+
+def main():
+    # TODO: initialize sensors as needed; control flow between safety and on guard modes.
+    pass
+
+def on_guard():
+    # TODO: see on guard flowchart
+    # Need to develop intrusion detection algorithm
+    pass
+
+def safety():
+    # TODO: see safety flowchart
+    pass
+
+if __name__ == '__main__':
+    main()
+    
