c1bce0ceeb874a8a5f423edb154ad110e4308a10
Juan <juanpaez@utexas.edu>
2021-02-04 17:51:45 -0500

    vibration module

--- Modules/Vibration/vibrationsensor.py
+++ Modules/Vibration/vibrationsensor.py
@@ -2,26 +2,26 @@ import RPi.GPIO as GPIO
 import time
 from datetime import datetime
 
-class MotionSensor:
+class VibrationSensor:
     def __init__(self, input_pin=11):
         self.input_pin = input_pin
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(self.input_pin, GPIO.IN)
 
-    def is_moving(self):
+    def is_vibrating(self):
         i=GPIO.input(self.input_pin)
         if i == 0:
-            print(f'No motion detected at {datetime.now()}')
+            print(f'No vibration detected at {datetime.now()}')
             return False
         elif i == 1:
-            print(f'Motion detected at {datetime.now()}')
+            print(f'Vibration detected at {datetime.now()}')
             return True
 
 
 if __name__ == '__main__':
-    motionsensor = MotionSensor()
+    vibrationsensor = VibrationSensor(input_pin=11)
     while(1):
-        print(motionsensor.is_moving())
+        print(vibrationsensor.is_vibrating())
         time.sleep(1)
 
