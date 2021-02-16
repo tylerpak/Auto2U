2adeabbeb425203d44a83726fc02d001a0ab9a9f
Juan Paez <juanfranciscopaez@gmail.com>
2021-02-15 18:11:02 -0600

    mock cloud endpoints and test server

--- Modules/client.py
+++ Modules/client.py
@@ -1,57 +1,187 @@
 import requests
 import json
-import jsonpickle
 import cv2
+import datetime
+import base64
 
 class Client:
-    def __init__(self):
-        pass
-
-    def send_video_frame(self, img_encoded):
+    def __init__(self, id):
+        '''ID should be unique identifier (e.g., sim card), should be send with every request'''
         pass
 
     def send_gps(self, lng, lat):
+        '''Request containing lattitude and longitude floats.'''
         pass
+
+    def send_video_frame(self, channel, start_datetime, img_encoded, img_num, total_num, fps):
+        '''Request sending a single frame from a video.
+        A video is uniquely identified by its channel and its start time.
+        channel: 0 (front camera) or 1 (back camera)
+        start_datetime: when the first frame was captured (note: all frames of the same video should use the same value)
+        img_encoded: .jpg image encoded using cv2.imencode()
+        img_num: frame number in video (1 to img_total)
+        total_num: total number of frames in the video
+        fps: image caputure rate in frames per second
+        '''
+        pass
+
+    def send_sos_warning(self):
+        '''Request letting AMS know we received SOS button push'''
+        pass
+
+    def send_low_battery_warning(self):
+        '''Request letting AMS know battery is low'''
+        pass
+
+    def query_play_alarm(self):
+        '''Request to be used to periodically query if the user has selected to play the alarm sound
+        after intrusion has been detected. Returns true or false.'''
+        pass
+
+    def query_all_clear(self):
+        '''Request to be used to periodically query if hte user has selected the all clear/ignore
+        option after intrusion has been detected. Returns true or false.'''
+        pass
+
+    def reset_play_alarm(self):
+        '''Request to reset alarm flag on AMS side.'''
+        pass
+
+    def reset_all_clear(self):
+        '''Request to reset all clear flag on AMS side.'''
+        pass
+
 
 class TestClient(Client):
     ### Works with local test server
-    def __init__(self):
-        pass
-
-    def send_video_frame(self, img_encoded):
-        # img_encoded should already be encoded with cv2.imencode('.jpg', img)
-        response = requests.post('http://localhost:5000/api/img', data=img_encoded.tostring(), headers={'content-type': 'image/jpeg'})
-        print(json.loads(response.text))
+    def __init__(self, id):
+        self.id = id
 
     def send_gps(self, lng, lat):
-        # img_encoded should already be encoded with cv2.imencode('.jpg', img)
-        data = jsonpickle.encode({'lng': lng, 'lat': lat})
-        response = requests.post('http://localhost:5000/api/gps', data=data, headers={'content-type': 'image/jpeg'})
+        data = json.dumps({
+            'id': self.id,
+            'lng': lng,
+            'lat': lat
+            })
+        response = requests.post('http://localhost:5000/api/gps', data=data)
+        print(json.loads(response.text))
+
+    def send_video_frame(self, channel, start_datetime, img_encoded, img_num, total_num, fps):
+        # Write .jpg ecoding as base64, then ascii
+        img_as_txt = base64.b64encode(img_encoded).decode('ascii')
+        data = json.dumps({
+            'id': self.id,
+            'channel': channel,
+            'video_timestamp': start_datetime.strftime("%m/%d/%Y, %H:%M:%S"),
+            'img_encoded': img_as_txt,
+            'img_num': img_num,
+            'total_num': total_num,
+            'fps': fps
+        })
+        response = requests.post('http://localhost:5000/api/img', data=data)
+        print(json.loads(response.text))
+        
+
+    def send_sos_warning(self):
+        data = json.dumps({
+            'id': self.id
+        })
+        response = requests.post('http://localhost:5000/api/sos', data=data)
+        print(json.loads(response.text))
+
+    def send_low_battery_warning(self):
+        data = json.dumps({
+            'id': self.id
+        })
+        response = requests.post('http://localhost:5000/api/lowbattery', data=data)
+        print(json.loads(response.text))
+
+    def query_play_alarm(self):
+        data = json.dumps({
+            'id': self.id
+        })
+        response = requests.get('http://localhost:5000/api/alarm', data=data)
+        print(json.loads(response.text))
+
+    def query_all_clear(self):
+        data = json.dumps({
+            'id': self.id
+        })
+        response = requests.get('http://localhost:5000/api/allclear', data=data)
+        print(json.loads(response.text))
+
+    def reset_play_alarm(self):
+        data = json.dumps({
+            'id': self.id
+        })
+        response = requests.put('http://localhost:5000/api/alarm', data=data)
+        print(json.loads(response.text))
+
+    def reset_all_clear(self):
+        data = json.dumps({
+            'id': self.id
+        })
+        response = requests.put('http://localhost:5000/api/allclear', data=data)
         print(json.loads(response.text))
 
 class CloudClient(Client):
     ### Works with AMS team's cloud server
-    def __init__(self):
-        # TODO
-        pass
-
-    def send_video_frame(self, img_encoded):
-        # TODO
-        pass
-
+    ### TODO: Populate with correct REST endpoint requests
     def send_gps(self, lng, lat):
-        # TODO
+        pass
+
+    def send_video_frame(self, channel, start_datetime, img_encoded, img_num, total_num, fps):
+        pass
+
+    def send_sos_warning(self):
+        pass
+
+    def send_low_battery_warning(self):
+        pass
+
+    def query_play_alarm(self):
+        pass
+
+    def query_all_clear(self):
+        pass
+
+    def reset_play_alarm(self):
+        '''Request to reset alarm flag on AMS side.'''
+        pass
+
+    def reset_all_clear(self):
+        '''Request to reset all clear flag on AMS side.'''
         pass
 
 
 ## TEST MAIN:
 if __name__ == '__main__':
-    # Sends image and gps coordinates using TestClient class to testserver.
-    # testserver should be running.
-    # When the image is sent, it returns its size in the response and saves it in black and white.
-    # When the gps coordinates are sent, it returns them in the response.
-    img = cv2.imread('test_img.jpg', 0)
-    _, img_enc = cv2.imencode('.jpg', img)
-    client = TestClient()
-    client.send_video_frame(img_enc)
+    # Test data transmission between client and server.
+    # If using TestClient, testserver should be running.
+    # If using CloudClient, AMS server should be running.
+    client = TestClient(42)
+    #client = CloudClient(42)
+
+    # Test GPS coordinate transmission
     client.send_gps(123.0, 456.0)
+
+    # Test image transmission with test_img.jpg
+    img = cv2.imread('test_img.jpg', 1)
+    _, img_enc = cv2.imencode('.jpg', img)
+    client.send_video_frame(0, datetime.datetime.now(), img_enc, 1, 20, 2)
+
+    # Test SOS
+    client.send_sos_warning()
+
+    # Test low battery
+    client.send_low_battery_warning()
+
+    # Test alarm/sound
+    client.query_play_alarm()
+    client.reset_play_alarm()
+
+    # Test all-clear
+    client.query_all_clear()
+    client.reset_all_clear()
+
+    
--- Modules/testserver.py
+++ Modules/testserver.py
@@ -1,46 +1,100 @@
 ## Credit: https://gist.github.com/kylehounslow/767fb72fde2ebdd010a0bf4242371594
 
 from flask import Flask, request, Response
-import jsonpickle
 import json
 import numpy as np
 import cv2
+import base64
 
 num = 1
 app = Flask(__name__)
 
-@app.route('/api/img', methods=['POST'])
-def image():
-    # Test API endpoint that simply saves image
-    global num
-    r = request
-    nparr = np.fromstring(r.data, np.uint8)
-    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    cv2.imwrite(f'output{num}.jpg', img)
-    num += 1
-
-    # build a response dict to send back to client
-    response = {'message': 'image received. size={}x{}'.format(img.shape[1], img.shape[0])
-                }
-    # encode response using jsonpickle
-    response_pickled = jsonpickle.encode(response)
-
-    return Response(response=response_pickled, status=200, mimetype="application/json")
-
 @app.route('/api/gps', methods=['POST'])
 def gps():
     # Test API endpoint that returns received gps coordinates
-    r = request
-    data = json.loads(r.data)
+    data = json.loads(request.data)
+    print(f'Received GPS coordinates: {data["lng"]}x{data["lat"]}\n')
+    response = json.dumps({'message': f'Received GPS coordinates: {data["lng"]}x{data["lat"]}'})
+    return Response(response=response, status=200, mimetype="application/json")
 
-    # build a response dict to send back to client
-    response = {'message': 'coordinates received={}x{}'.format(data['lng'], data['lat'])
-                }
-    # encode response using jsonpickle
-    response_pickled = jsonpickle.encode(response)
 
-    return Response(response=response_pickled, status=200, mimetype="application/json")
+@app.route('/api/img', methods=['POST'])
+def image():
+    # Test API endpoint that simply saves image
+    data = json.loads(request.data)
+    jpg_original = base64.b64decode(data['img_encoded'])
+    nparr = np.frombuffer(jpg_original, dtype=np.uint8)
+    img = cv2.imdecode(nparr, flags=1)
+    cv2.imwrite('test_output.jpg', img)
 
+    print(f'Received image:')
+    print(f'ID: {data["id"]}')
+    print(f'Timestamp: {data["video_timestamp"]}')
+    print(f'Channel: {data["channel"]}')
+    print(f'Size: {img.shape[1]}x{img.shape[0]}')
+    print(f'Image number: {data["img_num"]} of {data["total_num"]}')
+    print(f'')
+
+    response = json.dumps({'message': f'image received. size={img.shape[1]}x{img.shape[0]}'})
+    return Response(response=response, status=200, mimetype="application/json")
+
+
+@app.route('/api/sos', methods=['POST'])
+def sos():
+    # Handle SOS alert
+    data = json.loads(request.data)
+    print(f'Received SOS alert from ID={data["id"]}\n')
+    response = json.dumps({'message': f'Received SOS alert from ID={data["id"]}'})
+    return Response(response=response, status=200, mimetype="application/json")
+
+
+@app.route('/api/lowbattery', methods=['POST'])
+def lowbattery():
+    # Handle low battery alert
+    data = json.loads(request.data)
+    print(f'Received low battery alert from ID={data["id"]}\n')
+    response = json.dumps({'message': f'Received low battery alert from ID={data["id"]}'})
+    return Response(response=response, status=200, mimetype="application/json")
+
+
+@app.route('/api/alarm', methods=['GET', 'PUT'])
+def alarm():
+    # Handle alarm functionality
+    data = json.loads(request.data)
+    if request.method == 'GET':
+        # Always return true
+        print(f'Received alarm query from ID={data["id"]}\n')
+        response = json.dumps({
+            'message': f'Received alarm query from ID={data["id"]}',
+            'ret_val': True
+        })
+        return Response(response=response, status=200, mimetype="application/json")
+    elif request.method == 'PUT':
+        # Clear flag and acknowledge
+        print(f'Received alarm reset command from ID={data["id"]}\n')
+        response = json.dumps({'message': f'Reset alarm flag for ID={data["id"]}'})
+        return Response(response=response, status=200, mimetype="application/json")
+    return Response(status=500)
+
+
+@app.route('/api/allclear', methods=['GET', 'PUT'])
+def allclear():
+    # Handle alarm functionality
+    data = json.loads(request.data)
+    if request.method == 'GET':
+        # Always return true
+        print(f'Received all clear query from ID={data["id"]}\n')
+        response = json.dumps({
+            'message': f'Received all clear query from ID={data["id"]}',
+            'ret_val': True
+        })
+        return Response(response=response, status=200, mimetype="application/json")
+    elif request.method == 'PUT':
+        # Clear flag and acknowledge
+        print(f'Received all clear reset command from ID={data["id"]}\n')
+        response = json.dumps({'message': f'Reset all clear flag for ID={data["id"]}'})
+        return Response(response=response, status=200, mimetype="application/json")
+    return Response(status=500)
 
 if __name__ == "__main__":
     app.run(debug=True)
