b613764b9cf8ace7c77af7c682ded4fa2fc9361d
Juan Paez <juanfranciscopaez@gmail.com>
2021-03-24 16:18:16 -0500

    sound fix

--- Modules/sound.py
+++ Modules/sound.py
@@ -5,8 +5,8 @@ class SoundModule():
     def __init__(self, audio_file='alarm.wav'):
         self.file = audio_file
 
-    def play(self):
-        os.system("omxplayer alarm.wav")
+    def play(self, filename="Modules/alarm.wav"):
+        os.system(f"omxplayer {filename}")
         time.sleep(1)
 
 if __name__ == "__main__":
