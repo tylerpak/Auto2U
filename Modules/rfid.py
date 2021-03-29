45b83e080b590147d61eb69869ba8ad28eb6cd01
Juan Paez <juanfranciscopaez@gmail.com>
2021-03-28 19:22:22 -0500

    rfid

--- Modules/rfid.py
+++ Modules/rfid.py
@@ -1,6 +1,6 @@
-import RPi.GPIO as GPIO
 import time
 from datetime import datetime
+import sys, select
 
 class RFID:
     def __init__(self):
@@ -8,3 +8,14 @@ class RFID:
 
     def tag_detected(self):
         return False
+
+def input_with_timeout(timeout=0.5):
+    i, o, e = select.select( [sys.stdin], [], [], timeout)
+    if (i):
+        return sys.stdin.readline().strip()
+    else:
+        return ''
+
+if __name__ == '__main__':
+    time.sleep(5)
+    print(input_with_timeout(0.1))
