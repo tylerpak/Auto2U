e05d83ca76348db484d6dc6ca3034c3def5f9c8a
tylerpak <paktyler@utexas.edu>
2021-04-08 15:44:53 -0500

    rfid progress

--- Modules/rfid.py
+++ Modules/rfid.py
@@ -1,13 +1,24 @@
 import time
 from datetime import datetime
 import sys, select
+import serial
+import evdev
 
 class RFID:
-    def __init__(self):
-        pass
+    def __init__(self, event=0):
+        self.device = evdev.InputDevice(f'/dev/input/event{event}')
+        print(self.device)
+        self.curr_val = False
 
     def tag_detected(self):
-        return input_with_timeout(0.1) != ''
+        start = time.time()
+        detected = False
+        for event in self.device.read_loop():
+            print('Hello')
+            
+            
+        if count > 0:
+            return True
 
 def input_with_timeout(timeout=0.5):
     i, o, e = select.select( [sys.stdin], [], [], timeout)
@@ -25,11 +36,14 @@ def get_serial(ser):
 
 if __name__ == '__main__':
     rfid = RFID()
-    output = ''
-    ser = serial.Serial('/dev/hidraw3', 9600, timeout=1)
+    #output = ''
+    #ser = serial.Serial('/dev/input/event4', 9600, timeout=1)
+    #while True:
+    #    print ("----")
+    #    while output != "":
+    #        output = ser.readline().decode('UTF-8').strip()
+    #        print (output)
+    #        output = ""
+
     while True:
-        print ("----")
-        while output != "":
-            output = get_serial(ser)
-            print (output)
-            output = " "
+        print(rfid.tag_detected())
