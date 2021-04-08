23ec9676d82680ef5ccb80244a5d3bd3d2074214
Juan Paez <juanfranciscopaez@gmail.com>
2021-04-08 15:57:22 -0500

    rfid cont.

--- Modules/rfid.py
+++ Modules/rfid.py
@@ -1,23 +1,26 @@
 import time
 from datetime import datetime
 import sys, select
-import serial
-import evdev
+
 
 class RFID:
-    def __init__(self, event=0):
-        self.device = evdev.InputDevice(f'/dev/input/event{event}')
-        print(self.device)
-        self.curr_val = False
+
+    def __init__(self):
+        self.value = False
 
     def tag_detected(self):
-        start = time.time()
-        detected = False
-        for event in self.device.read_loop():
-            print('Hello')
+        
+        file = open("Modules/rfid_in.txt", 'w+')
+        rfid_tag = file.read()
+        if rfid_tag != '' and rfid_tag is not None:
+            print('Detected Tag')
+            self.value = self.value ^ True
+            file.truncate(0)
+        file.close()
+
+        return self.value
             
             
-        if count > 0:
-            return True
+        
 
 
